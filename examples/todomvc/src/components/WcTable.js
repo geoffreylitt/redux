6659963533e7fb7090cc989275a2ec055f3862a5
Geoffrey Litt <gklitt@gmail.com>
2020-04-06 16:01:26 -0400

    add a basic debug panel

--- examples/todomvc/src/components/WcTable.js
+++ examples/todomvc/src/components/WcTable.js
@@ -1,15 +1,38 @@
 import React from 'react'
 
-const WcTable = ({sortOrder, actions}) => {
-  let divStyle = {
-    position: "fixed",
-    height: "150px",
-    left: 0,
-    bottom: 0,
-    width: "100vw",
-    backgroundColor: "white"
-  }
+const divStyle = {
+  position: "fixed",
+  height: "300px",
+  left: 0,
+  bottom: 0,
+  width: "100vw",
+  backgroundColor: "white",
+  fontSize: "14px",
+  fontWeight: "normal"
+}
 
+const todosTable = (todos) => {
+  return <table border="1">
+      <thead style={{display: "table-header-group", textAlign: "left"}}>
+        <tr>
+          <th>id</th>
+          <th>text</th>
+          <th>completed</th>
+        </tr>
+      </thead>
+      <tbody>
+        {todos.map(todo => {
+          return <tr>
+            <td>{todo.id}</td>
+            <td>{todo.text}</td>
+            <td>{todo.completed ? "true" : "false"}</td>
+          </tr>
+        })}
+      </tbody>
+    </table>
+}
+
+const WcTable = ({todos, sortedTodos, sortOrder, actions}) => {
   let sortByText = () => {
     if (!sortOrder.column) {
       actions.sortTable("text", "ASC");
@@ -21,13 +44,25 @@ const WcTable = ({sortOrder, actions}) => {
   }
 
   return <div style={divStyle}>
-    <h2>Sorting control</h2>
+    <div style={{float: "left", width: "33vw"}}>
+      <h2>todos</h2>
+      {todosTable(todos)}
+    </div>
 
-    <div>Sort column: {sortOrder.column}</div>
-    <div>Sort direction: {sortOrder.direction}</div>
+    <div style={{float: "left", width: "33vw"}}>
+      <h2>sorted/filtered view</h2>
+      {todosTable(sortedTodos)}
+    </div>
 
-    <div>
-      <button style={{textDecoration: "underline"}} onClick={sortByText}>Sort by text</button>
+    <div style={{float: "left", width:"33vw"}}>
+      <h2>Sort Control</h2>
+
+      <div>Sort column: <strong>{sortOrder.column}</strong></div>
+      <div>Sort direction: <strong>{sortOrder.direction}</strong></div>
+
+      <div>
+        <button style={{backgroundColor: "#0f4d92", color: "white", padding: 10, borderRadius: 5}} onClick={sortByText}>Sort by text</button>
+      </div>
     </div>
   </div>
 }
--- examples/todomvc/src/containers/WcTable.js
+++ examples/todomvc/src/containers/WcTable.js
@@ -2,10 +2,11 @@ import { connect } from 'react-redux'
 import * as TodoActions from '../actions'
 import { bindActionCreators } from 'redux'
 import WcTable from '../components/WcTable'
-import { getCompletedTodoCount } from '../selectors'
-
+import { getSortedTodos } from '../selectors'
 
 const mapStateToProps = state => ({
+  todos: state.todos,
+  sortedTodos: getSortedTodos(state),
   sortOrder: state.__wildcard__.sort
 })
 
