e27e38e73afb267bf1bdb6b363c48edcbd69e991
Geoffrey Litt <gklitt@gmail.com>
2020-04-07 17:12:42 -0400

    fix

--- examples/todomvc/src/components/App.js
+++ examples/todomvc/src/components/App.js
@@ -3,14 +3,13 @@ import Header from '../containers/Header'
 import MainSection from '../containers/MainSection'
 import WcTable from '../wildcard/WcTableContainer'
 
+// Adding Wildcard here won't be necessary in the real extension version;
+// we're just showing the table within the app's component hierarchy
+// for now for convenience.
 const App = () => (
   <div>
     <Header />
     <MainSection />
-
-    // This won't be necessary in the real extension version;
-    // we're just showing the table within the app's component hierarchy
-    // for now for convenience.
     <WcTable />
   </div>
 )
